ac9378e121e1dee0d520f82e02ae53b50d5e96b4
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-07-02 18:13:53 +0530

    n = int(input()) x = str(n) op = [x] for i in range(len(x)):     if x[i] == "9":         z = x[:i] + "6" + x[1+i:]     elif x[i] == "6":         z = x[:i] + "9" + x[1+i:]     op.append(z) op.sort() print(op[-1])

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,4 +1,11 @@
-def max_69(self, num: int) -> int:
-    return str(num).replace('6','9',1)
-n=input()
-print(max_69(1,n))
+n = int(input())
+x = str(n)
+op = [x]
+for i in range(len(x)):
+    if x[i] == "9": 
+        z = x[:i] + "6" + x[1+i:]
+    elif x[i] == "6":
+        z = x[:i] + "9" + x[1+i:]
+    op.append(z)
+op.sort()
+print(op[-1])
