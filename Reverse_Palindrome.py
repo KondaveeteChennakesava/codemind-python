a3dfe012818f312292b614451fbd9606ec752084
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-10-12 14:41:32 +0530

    def res(n):     rev=0     temp=n     while n>0:         d=n%10         n=n//10         rev=rev*10+d     return rev n=int(input()) sum=0 while True:     sum=n+res(n)     if sum==res(sum):         print(sum)         break     else:         n=sum         continue

--- Reverse_Palindrome.py
+++ Reverse_Palindrome.py
@@ -1,17 +1,18 @@
-def palin(n):
-    v=0
-    s=n
-    while n!=0:
-        x=n%10
-        v=v*10+x
-        n//=10
-    d=s+v
-    e=str(d)
-    if str(d)==e[::-1]:
-        print(d)
+def res(n):
+    rev=0
+    temp=n
+    while n>0:
+        d=n%10
+        n=n//10
+        rev=rev*10+d
+    return rev
+n=int(input())
+sum=0
+while True:
+    sum=n+res(n)
+    if sum==res(sum):
+        print(sum)
+        break
     else:
-        palin(d)
-        
-        
-a=int(input())
-palin(a)
+        n=sum
+        continue
