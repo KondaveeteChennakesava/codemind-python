a8ee76e019ed93aef2ccb5db838405675c3db1b8
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-10-13 14:43:59 +0530

    import math def pr(a):     if a>1:         for i in range(2,int(math.sqrt(a))+1):             if a%i==0:                 return False                 break     elif a==1:         return False     return True
    a=int(input())
    b=int(input())
    c=0
    for i in range(a,b+1):
        if pr(i)==True:
            c+=1
    print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,17 +1,18 @@
-def prime(n):
-    if n==1:
-        return 0
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
-            return 0
-    return 1
+import math
+def pr(a):
+    if a>1:
+        for i in range(2,int(math.sqrt(a))+1):
+            if a%i==0:
+                return False
+                break
+    elif a==1:
+        return False
+    return True
+
 a=int(input())
 b=int(input())
 c=0
 for i in range(a,b+1):
-    if prime(i):
+    if pr(i)==True:
         c+=1
-if c==0:
-    print('-1')
-else:
-    print(c)
+print(c)
