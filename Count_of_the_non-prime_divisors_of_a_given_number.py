28e71669f646d2b6ad517e996d80f2e762ab2cc3
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-10-14 11:26:06 +0530

    import math def pr(a):     if a>1:         for i in range(2,int(math.sqrt(a))+1):             if a%i==0:                 return False                 break     elif a==1:         return False     return True
    n=int(input())
    c=0
    for i in range(1,n+1):
        if n%i==0 and pr(i)!=True:
            c+=1
    print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,11 +1,17 @@
+import math
+def pr(a):
+    if a>1:
+        for i in range(2,int(math.sqrt(a))+1):
+            if a%i==0:
+                return False
+                break
+    elif a==1:
+        return False
+    return True
+
 n=int(input())
-s=0
+c=0
 for i in range(1,n+1):
-    if(n%i==0):
-        c=0
-        for j in range(1,i+1):
-            if(i%j==0):
-                c+=1
-        if(c!=2):
-            s+=1
-print(s)
+    if n%i==0 and pr(i)!=True:
+        c+=1
+print(c)
