a8e737e54edced580c94dd0f49efec319fa797e0
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-10-30 12:07:41 +0530

    import math def pr(a):     if a>1:         for i in range(2,int(math.sqrt(a)+1)):             if a%i==0:                 return False                 break     elif a==1:         return False     return True a=int(input()) b=int(input()) for i in range(a,b+1):     if pr(i)==True:         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,14 +1,15 @@
-def fun(n):
-    c=0
-    for i in range(1,n+1):
-        if(n%i==0):
-            c+=1
-    if c==2:
-        return 1
-    else:
-        return 0
+import math
+def pr(a):
+    if a>1:
+        for i in range(2,int(math.sqrt(a)+1)):
+            if a%i==0:
+                return False
+                break
+    elif a==1:
+        return False
+    return True
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    if(fun(i)):
+    if pr(i)==True:
         print(i)
