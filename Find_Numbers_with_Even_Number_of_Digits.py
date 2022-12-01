bc4d6df49b429525f7180b0bf09c037f4d95c55b
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-12-01 18:08:27 +0530

    def ev(a):     c=0     while a>0:         r=a%10         a//=10         c+=1     if c%2==0:         return True     else:         return False a=int(input()) l=list(map(int,input().split())) cnt=0 for i in l:     if ev(i)==True:         cnt+=1 print(cnt)

--- Find_Numbers_with_Even_Number_of_Digits.py
+++ Find_Numbers_with_Even_Number_of_Digits.py
@@ -1,13 +1,17 @@
-n=int(input())
-a=list(map(int,input().split()))
-c=0;
-m=0
-for i in range(len(a)):
-  c=0
-  while(a[i]!=0):
-   d=a[i]%10
-   a[i]=a[i]//10
-   c+=1
-  if(c%2==0):
-     m+=1
-print(m)
+def ev(a):
+    c=0
+    while a>0:
+        r=a%10
+        a//=10
+        c+=1
+    if c%2==0:
+        return True
+    else:
+        return False
+a=int(input())
+l=list(map(int,input().split()))
+cnt=0
+for i in l:
+    if ev(i)==True:
+        cnt+=1
+print(cnt)
