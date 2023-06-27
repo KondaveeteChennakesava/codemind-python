d3904960345986a048b9648028ac33ff3a67b13b
Kondaveete Chennakesava <96309319+KondaveeteChennakesava@users.noreply.github.com>
2023-06-27 15:10:17 +0530

    n,m = map(int,input().split()) m1,m2 = [],[] for i in range(n):     m1.append(list(map(int,input().split()))) for i in range(m):     tl = []     for j in range(n):         tl.append(m1[j][i])     m2.append(tl) for i in m2:     print(sum(i),end=' ')

--- Column_wise_sum_of_a_matrix.py
+++ Column_wise_sum_of_a_matrix.py
@@ -1,8 +1,11 @@
-n,m=map(int,input().split())
-s=[0]*m
-for j in range(n):
-    m=list(map(int,input().split()))
-    for i in range(len(m)):
-        s[i]=s[i]+m[i]
-for k in s:
-    print(k,end=' ')
+n,m = map(int,input().split())
+m1,m2 = [],[]
+for i in range(n):
+    m1.append(list(map(int,input().split())))
+for i in range(m):
+    tl = []
+    for j in range(n):
+        tl.append(m1[j][i])
+    m2.append(tl)
+for i in m2:
+    print(sum(i),end=' ')
