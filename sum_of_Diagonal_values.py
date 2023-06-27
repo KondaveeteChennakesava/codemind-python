8ddbdef99a25af4a5ff32bddaac31b025367a7a9
Kondaveete Chennakesava <96309319+KondaveeteChennakesava@users.noreply.github.com>
2023-06-27 15:32:41 +0530

    n,m = map(int,input().split()) m1,m2 = [],[] for i in range(n):     m1.append(list(map(int,input().split()))) cnt = 0 for i in range(n):     for j in range(m):         if(i==j or i+j==n-1):             cnt += m1[i][j] print(cnt)

--- sum_of_Diagonal_values.py
+++ sum_of_Diagonal_values.py
@@ -1,8 +1,10 @@
-n,x=map(int,input().split())
-s=0
-for j in range(n):
-    m=list(map(int,input().split()))
-    for i in range(len(m)):
-        if i==j or i==len(m)-1-j:
-            s=s+m[i]
-print(s)
+n,m = map(int,input().split())
+m1,m2 = [],[]
+for i in range(n):
+    m1.append(list(map(int,input().split())))
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if(i==j or i+j==n-1):
+            cnt += m1[i][j]
+print(cnt)
