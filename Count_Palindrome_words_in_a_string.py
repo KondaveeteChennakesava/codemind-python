1738411e7a9e57112ebcdcab28d25b63b0d65ae7
Kondaveete Chennakesava <96309319+KondaveeteChennakesava@users.noreply.github.com>
2023-06-30 13:58:24 +0530

    def pl(a):     a = a.lower()     k = a[::-1]     if a == k:         return True     return False n = input().split() cnt = 0 for i in n:     if pl(i):         cnt += 1 print(cnt) #print(pl('Madam'))

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,7 +1,13 @@
-s=input()
-s=s.split()
-k=0
-for x in s:
-    if(x.casefold()==x.casefold()[::-1]):
-        k+=1
-print(k)        
+def pl(a):
+    a = a.lower()
+    k = a[::-1]
+    if a == k:
+        return True
+    return False
+n = input().split()
+cnt = 0
+for i in n:
+    if pl(i):
+        cnt += 1
+print(cnt)
+#print(pl('Madam'))
