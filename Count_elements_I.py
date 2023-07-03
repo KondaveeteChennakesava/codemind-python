af943dd2a8241aaa6637bc56c9792fe3802c0cf6
Kondaveete Chennakesava <96309319+KondaveeteChennakesava@users.noreply.github.com>
2023-07-03 11:04:27 +0530

    a,b = map(int,input().split()) a1 = list(map(int,input().split())) b1 = list(map(int,input().split())) res = set(a1).intersection(b1) print(len(res))

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,10 +1,5 @@
-m,n=map(int,input().split())
-l=[]
-a=list(map(int,input().split()))[:m]
-b=list(map(int,input().split()))[:n]
-for i in range(0,len(a)):
-    for j in range(0,len(b)):
-        if a[i]==b[j]:
-            l.append(int(a[i]))
-l=list(set(l))
-print(len(l))
+a,b = map(int,input().split())
+a1 = list(map(int,input().split()))
+b1 = list(map(int,input().split()))
+res = set(a1).intersection(b1)
+print(len(res))
